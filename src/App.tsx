cbe7612b6b02c65f1da4892beb7902d77dc22a25
Chloe Brown <chloe.brown65@thehutgroup.com>
2023-03-07 13:43:53 +0000

    added dropdown box

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { DropDownBox } from "./Components/DropDownBox/DropDownBox";
 import { Header } from "./Components/Header/Header";
 
 import { ItemList } from "./Components/ItemList/ItemList";
 
 function App() {
+  const [selectedOption, setSelectedOption] = useState("ALL");
+  const types = ["ALL", "TABLE", "CHAIR"];
+
   return (
     <div className="App">
       <Header />
+      <DropDownBox
+        selectedOption={selectedOption}
+        setSelectedOption={setSelectedOption}
+        options={types}
+      />
       <ItemList />
     </div>
   );
