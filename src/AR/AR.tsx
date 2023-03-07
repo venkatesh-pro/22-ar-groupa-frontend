3c90aeca8f16df6f066906f09e60ae08d52b9602
Drew <drew.soul61@thehutgroup.com>
2023-03-07 14:52:17 +0000

    added box rotation in AR

--- src/AR/AR.tsx
+++ src/AR/AR.tsx
@@ -1,8 +1,18 @@
 import { Canvas } from "@react-three/fiber";
-import { XR, XRButton } from "@react-three/xr";
-import Box from "./Box";
-
+import { Interactive, XR, XRButton } from "@react-three/xr";
+import { useState } from "react";
+import { A11y, A11yAnnouncer } from "@react-three/a11y";
+import Cube from "./Box";
+import RightBox from "./RotateRightButton";
+import * as THREE from "three";
+import { Flex, Box } from "@react-three/flex";
 const AR = () => {
+  const [rotateClick, setRotateClick] = useState(false);
+
+  const handleSelect = () => {
+    setRotateClick(!rotateClick);
+  };
+
   return (
     <div>
       <XRButton mode="AR" />
@@ -10,9 +20,13 @@ const AR = () => {
         <XR>
           <ambientLight />
           <pointLight position={[10, 10, 10]} />
-          <Box position={[-1.2, 0, 0]} />
-          <Box position={[1.2, 0, 0]} />
-          <Box position={[1.2, -3, 0]} />
+          <Interactive onSelect={handleSelect}>
+            <Cube
+              position={new THREE.Vector3(0, 0, -2)}
+              rotateClick={rotateClick}
+              setRotateClick={setRotateClick}
+            />
+          </Interactive>
         </XR>
       </Canvas>
     </div>
