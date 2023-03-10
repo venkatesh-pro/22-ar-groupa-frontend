7cd69d61bd1e31d41268461f20416f904d98dda0
Drew <drew.soul61@thehutgroup.com>
2023-03-10 15:26:37 +0000

    added AR 'buttons' to move item

--- src/AR/AugmentedReality.tsx
+++ src/AR/AugmentedReality.tsx
@@ -1,16 +1,18 @@
 import { Canvas } from "@react-three/fiber";
-import { ARButton, XR, useHitTest } from "@react-three/xr";
+import { ARButton, XR, useHitTest, Interactive, useXR } from "@react-three/xr";
 import Box from "./Box";
 import { MarioFloorLamp } from "./3D_models/MarioFloorLamp";
+import { MilleniumFalcon } from "./3D_models/MilleniumFalcon";
 import s from "./AR.styles";
-import React from "react";
-// import { useRef } from "react";
-// import { Matrix4, Mesh } from "three";
-
-const TestBox = React.forwardRef(
-  (props: JSX.IntrinsicElements["mesh"], ref) => {
-    const boxRef = React.useRef<THREE.Mesh>(null);
-    useHitTest((hitMatrix: THREE.Matrix4, hit: XRHitTestResult) => {
+import React, { useEffect, useRef, useState } from "react";
+export function AugmentedReality() {
+  const [rotate, setRotate] = useState(0);
+  const [x, setX] = useState(0);
+  const [y, sety] = useState(0);
+  const [z, setZ] = useState(-4);
+  const Item = React.forwardRef((props: JSX.IntrinsicElements["mesh"], ref) => {
+    const boxRef = React.useRef<THREE.Mesh>(null!);
+    useHitTest((hitMatrix) => {
       if (boxRef.current) {
         hitMatrix.decompose(
           boxRef.current.position,
@@ -18,27 +20,52 @@ const TestBox = React.forwardRef(
           boxRef.current.scale
         );
       }
-      // const Transform = new XRRigidTransform()
-      // hit.createAnchor(Transform).then((anchor) => {
-      //   console.log(anchor)
-      // }, (error) => {
-      //   console.error(`Could not create anchor: ${error}`);
-      // }
     });
 
-    return <MarioFloorLamp red={boxRef} {...props} position={[0, 0, -2]} />;
-  }
-);
+    return <MarioFloorLamp ref={boxRef} position={[x, y, z]} rotate={rotate} />;
+  });
 
-export function AugmentedReality() {
   return (
     <s.canvasContainer>
       <ARButton sessionInit={{ requiredFeatures: ["hit-test"] }} />
-      <Canvas camera={{ position: [0, 0, 2] }}>
+      <Canvas>
         <XR>
           <ambientLight />
           <pointLight position={[10, 10, 10]} />
-          <TestBox />
+          <Interactive
+            onSelect={() => (rotate === 0 ? setRotate(0.05) : setRotate(0))}
+          >
+            <Item />
+          </Interactive>
+          <Interactive
+            onSelect={() => {
+              setX(x + 0.1);
+            }}
+          >
+            <Box position={[0.3, -3, -2]} />
+          </Interactive>
+          <Interactive
+            onSelect={() => {
+              setX(x - 0.1);
+            }}
+          >
+            <Box position={[-0.3, -3, -2]} />
+          </Interactive>
+          <Interactive
+            onSelect={() => {
+              setZ(z + 0.1);
+            }}
+          >
+            <Box position={[0, -3, -1.7]} />
+          </Interactive>
+          <Interactive
+            onSelect={() => {
+              setZ(z - 0.1);
+            }}
+          >
+            <Box position={[0, -3, -2.3]} />
+          </Interactive>
+          <Box position={[0, -3, -2]} />
         </XR>
       </Canvas>
     </s.canvasContainer>
