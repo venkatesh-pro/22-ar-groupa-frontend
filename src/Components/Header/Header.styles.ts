09ad896e0e9381e4ea6ace11d5fbe2f0857ba0b8
Chloe Brown <chloe.brown65@thehutgroup.com>
2023-03-10 10:42:27 +0000

    changes to header

--- src/Components/Header/Header.styles.ts
+++ src/Components/Header/Header.styles.ts
@@ -1,24 +1,58 @@
 import styled from "styled-components";
 
 const headerContainer = styled.div`
-  font-family: Impact;
-  color: #d4a373;
+  width: 100%;
+  height: 80px;
+  background-color: black;
   display: flex;
-  justify-content: space-between;
   align-items: center;
 `;
 const header = styled.h1`
-  margin: 40px;
+  justify-content: flex-start;
+  color: white;
+  font-size: 30px;
+  margin-left: 10px;
+`;
+const logo = styled.h1`
+  color: red;
+  font-size: 35px;
 `;
 
 const button = styled.button`
   display: flex;
+  justify-content: end;
+  align-items: center;
+  gap: 15px;
+  margin-left: 25px;
+  text-decoration: none;
+  color: white;
+  margin-left: 20px;
+  font-size: 25px;
+
+  &:hover {
+    text-decoration: none;
+    color: white;
+    margin-left: 20px;
+    font-size: 25px;
+    border: 1px solid white;
+    padding: 5px;
+    border-radius: 15px;
+  }
+
+  &:active {
+    background-color: white;
+    color: black;
+    border: 1px solid white;
+    padding: 5px;
+    border-radius: 15px;
+  }
 `;
 
 const styles = {
   header,
   headerContainer,
   button,
+  logo,
 };
 
 export default styles;
