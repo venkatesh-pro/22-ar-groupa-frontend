316454fc3f8056a1cfdd068268d252d0b6230a27
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2023-03-13 16:22:23 +0000

    Product card kind of works, but no Header element and no styling

--- src/Components/ProductCard/ProductCard.tsx
+++ src/Components/ProductCard/ProductCard.tsx
@@ -1,29 +1,54 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { item } from "../Item/Item";
+import s from "./ProductCard.styles";
 
-const ProductCard = () => {
+// const ProductCard: React.FC<item> = ({
+//   product_name,
+//   imagePath,
+//   product_desc,
+//   product_id,
+// }) => {
+
+const ProductCard: React.FC = () => {
   const { id } = useParams();
+  const [product, setProduct] = useState<item | null>(null);
 
-  //   const useGetItemById = (): any => {
   useEffect(() => {
     fetch(`/api/products/${id}`, {
       method: "GET",
     })
       .then((response) => {
-        console.log(response.status);
-        console.log(response);
+        console.log(response.text);
         return response.json();
+      })
+      .then((response) => {
+        setProduct(response);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [id]);
 
   return (
     <div>
-      <h1>This is product card TITLE</h1>
       Id: {id}
-      {/* {product_name} */}
+      {product ? (
+        <div>
+          <s.itemImage
+            src={product.imagePath}
+            alt={product.product_name}
+          ></s.itemImage>
+          <s.itemText>Product name: {product.product_name}</s.itemText>
+          <s.itemText>Description: {product.product_desc}</s.itemText>
+
+          {/* <p>Price: {product.product_price}</p> */}
+          <button>Add to basket</button>
+          <button>Try AR</button>
+        </div>
+      ) : (
+        <p>Product not found</p>
+      )}
     </div>
   );
 };
