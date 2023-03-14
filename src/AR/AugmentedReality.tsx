7e9c03f63a003f0f8d124ca8b6ea4aa8c832f840
Alan Kerby <Alan.Kerby40@thehutgroup.com>
2023-03-14 16:27:04 +0000

    changed name to models

--- src/AR/AugmentedReality.tsx
+++ src/AR/AugmentedReality.tsx
@@ -1,7 +1,7 @@
 import { Canvas } from "@react-three/fiber";
 import { ARButton, XR} from "@react-three/xr";
 import Box from "./Box";
-import { ModelMap } from "./3D_models/ModelMap";
+import { ModelMap } from "./3D_models/models";
 import s from "./AR.styles";
 import { Scene } from "./3D_models/Scene";
 import { MarioFloorLamp } from "./3D_models/MarioFloorLamp";
@@ -16,7 +16,7 @@ export function AugmentedReality() {
           <pointLight position={[1, 1, 1]} />
           <Box position={[3, 0, -10]} />
           {/* <MarioFloorLamp/> */}
-          <Scene threeD={ModelMap[0]}/>
+          <Scene threeD={ModelMap[2]}/>
         </XR>
       </Canvas>
       <ARButton />
