7fc2a3c4278fdefb1a79a13574acbdefd93cf897
Chloe Brown <chloe.brown65@thehutgroup.com>
2023-03-15 12:01:17 +0000

    styled login page

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,11 +1,33 @@
 import { Header } from "../../Components/Header/Header";
 import s from "./Login.styles";
+import { RiCloseCircleFill } from "react-icons/ri";
 
 export const Login: React.FC = () => {
   return (
     <s.loginContainer>
       <Header />
-      Login
+      <s.loginHeader>Login</s.loginHeader>
+      <s.loginBox>
+        <s.userDetailWrapper>
+          <s.userDetailContainer key={"username"}>
+            <s.userDetailText key={"username-text"}>UserName:</s.userDetailText>
+            <s.userDetailBox
+              key={"username-input"}
+              type="text"
+            ></s.userDetailBox>
+          </s.userDetailContainer>
+          <s.userDetailContainer key={"password"}>
+            <s.userDetailText key={"password-text"}>Password:</s.userDetailText>
+            <s.userDetailBox
+              key={"password-input"}
+              type="text"
+            ></s.userDetailBox>
+          </s.userDetailContainer>
+        </s.userDetailWrapper>
+        <s.cancelButton to="/">
+          <RiCloseCircleFill size={25} />
+        </s.cancelButton>
+      </s.loginBox>
     </s.loginContainer>
   );
 };
