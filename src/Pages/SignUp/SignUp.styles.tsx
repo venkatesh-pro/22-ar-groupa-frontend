decf2a8962388ec5e2662bb368bdaa283a2fe317
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2023-03-15 11:48:04 +0000

    Basic SignUp form added

--- src/Pages/SignUp/SignUp.styles.tsx
+++ src/Pages/SignUp/SignUp.styles.tsx
@@ -1,7 +1,31 @@
 import styled from "styled-components";
 
 const signUpContainer = styled.div``;
+const form = styled.form`
+  margin: 10px 10px;
+  border: 1px solid black;
+  border-radius: 15px;
+`;
 
-const styles = { signUpContainer };
+const h1 = styled.h1`
+  color: pink;
+  text-align: center;
+`;
+
+const div_form = styled.div`
+  border: 1px solid red;
+`;
+
+const div_field = styled.div`
+  color: green;
+  border: 1px solid green;
+  margin: 10px auto;
+`;
+
+const button_auth = styled.button`
+  background-color: aqua;
+`;
+
+const styles = { signUpContainer, form, h1, div_form, div_field, button_auth };
 
 export default styles;
--- src/Pages/SignUp/SignUp.tsx
+++ src/Pages/SignUp/SignUp.tsx
@@ -5,7 +5,28 @@ export const SignUp: React.FC = () => {
   return (
     <s.signUpContainer>
       <Header />
-      Sign Up
+      <s.form>
+        <s.h1>Sign Up</s.h1>
+        <s.div_form>
+          <s.div_field>
+            <label>Email:</label>
+            <input type="email" name="email" placeholder="Email" />
+          </s.div_field>
+          <s.div_field>
+            <label>Password:</label>
+            <input type="password" name="password" placeholder="Password" />
+          </s.div_field>
+          <s.div_field>
+            <label>Repeat Password:</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Repeat Password"
+            />
+          </s.div_field>
+          <s.button_auth>Create Account</s.button_auth>
+        </s.div_form>
+      </s.form>
     </s.signUpContainer>
   );
 };
