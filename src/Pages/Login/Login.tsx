097f3224377c254a00706c3b9dc4560225e202b0
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2023-03-17 15:14:06 +0000

    Login page render test

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -8,18 +8,26 @@ export const Login: React.FC = () => {
   return (
     <s.loginContainer>
       <Header />
-      <s.loginHeader>Login</s.loginHeader>
+      <s.loginHeader data-testid="login-header">Login</s.loginHeader>
       <s.loginBox>
         <s.cancelButton to="/">
           <RiCloseCircleFill size={25} />
         </s.cancelButton>
         <s.userDetailContainer key={"username"}>
           <s.userDetailText key={"username-text"}>UserName:</s.userDetailText>
-          <s.userDetailBox key={"username-input"} type="text"></s.userDetailBox>
+          <s.userDetailBox
+            key={"username-input"}
+            type="text"
+            placeholder="Email"
+          ></s.userDetailBox>
         </s.userDetailContainer>
         <s.userDetailContainer key={"password"}>
           <s.userDetailText key={"password-text"}>Password:</s.userDetailText>
-          <s.userDetailBox key={"password-input"} type="text"></s.userDetailBox>
+          <s.userDetailBox
+            key={"password-input"}
+            type="text"
+            placeholder="Password"
+          ></s.userDetailBox>
         </s.userDetailContainer>
         <s.loginButton>LOGIN</s.loginButton>
       </s.loginBox>
