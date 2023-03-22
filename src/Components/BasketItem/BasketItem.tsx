e1e9c576719e427b01a5af1df29e6a0b3668ba31
Ruchika Scott <ru.scott24@gmail.com>
2023-03-22 12:46:50 +0000

    Trying to style

--- src/Components/BasketItem/BasketItem.tsx
+++ src/Components/BasketItem/BasketItem.tsx
@@ -2,8 +2,8 @@ import React, { useContext } from "react";
 import { item } from "../Item/Item";
 import s from "./BasketItem.styles";
 import { RiDeleteBin2Line } from "react-icons/ri";
-
 import { BasketStateContext } from "../../Pages/Basket/Basket";
+import { AddToBasket } from "../../Functions/AddToBasket";
 
 interface props {
   item: item;
@@ -25,13 +25,42 @@ export const BasketItem: React.FC<props> = ({ item, number }) => {
         setError(true);
       });
   };
+
+  const handleSingleDelete = () => {
+    console.log("ADDED TO BASKET");
+    setLoading(true);
+    fetch(`basket/1/single/delete?productId=${item.product_id}`, {
+      method: "DELETE",
+    })
+      .finally(() => {
+        setLoading(false);
+        window.location.reload();
+      })
+      .catch(() => {
+        setError(true);
+      });
+  };
+
+  const handleAdd = () => {
+    if (!(item.product_id === undefined)) {
+      AddToBasket(setLoading, setError, item.product_id);
+      window.location.reload();
+    }
+  };
   return (
     <s.cartItem>
       <s.cartItemImg src={item.imagePath} />
       <s.itemDescription>
         <s.itemName>{item.product_name}</s.itemName>
         <s.itemPrice>Price: £{item.product_price}</s.itemPrice>
-        <s.itemPrice> Quantity : {number}</s.itemPrice>
+        <s.incrementContainer>
+          <s.incrementButton onClick={handleSingleDelete}>
+            {" "}
+            -{" "}
+          </s.incrementButton>
+          <s.itemPrice> Quantity : {number}</s.itemPrice>
+          <s.incrementButton onClick={handleAdd}> + </s.incrementButton>
+        </s.incrementContainer>
       </s.itemDescription>
       <s.deleteButton onClick={handleDelete}>
         <RiDeleteBin2Line size={32} />
