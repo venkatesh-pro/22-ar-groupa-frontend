0eb9944fa8211d5fb1214a639e3c1a154a50c986
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2023-03-24 14:02:39 +0000

    Login and Signup style changed

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -28,90 +28,90 @@ export const Header = (props: customer) => {
   if (mobileView) {
     return (
       <div>
-      <s.topBanner/>
-      <s.headerWrapper>
-        <s.headerContainer>
-          <s.header>
-            THE H<s.logo>AR</s.logo>T.
-          </s.header>
-          <s.menuButton onClick={handleClick}>
-            <RiMenuLine size={32} />
-          </s.menuButton>
-        </s.headerContainer>
-        {toggle && (
-          <s.menuContainer>
-            <s.button to="/">Home</s.button>
-            {options.map((op) => (
-              <s.button key={op} to={`/${op}`}>
-                {op}
+        <s.topBanner />
+        <s.headerWrapper>
+          <s.headerContainer>
+            <s.header>
+              THE H<s.logo>AR</s.logo>T.
+            </s.header>
+            <s.menuButton onClick={handleClick}>
+              <RiMenuLine size={32} />
+            </s.menuButton>
+          </s.headerContainer>
+          {toggle && (
+            <s.menuContainer>
+              <s.button to="/">Home</s.button>
+              {options.map((op) => (
+                <s.button key={op} to={`/${op}`}>
+                  {op}
+                </s.button>
+              ))}
+              {props.customerId === null ? (
+                <div>
+                  <s.button to="/signup">Sign Up</s.button>
+                  <s.button to="/login">Log In</s.button>
+                </div>
+              ) : (
+                <div>
+                  <s.button
+                    to="/App"
+                    onClick={() => {
+                      props.setCustomerId(null);
+                    }}
+                  >
+                    Log out
+                  </s.button>{" "}
+                  <s.header>{props.customerId}</s.header>
+                </div>
+              )}
+              <s.button to="/basket">
+                <RiShoppingCart2Line size={32} />
               </s.button>
-            ))}
-            {props.customerId === null ? (
-              <div>
-                <s.button to="/signup">Sign Up</s.button>
-                <s.button to="/login">Log in</s.button>
-              </div>
-            ) : (
-              <div>
-                <s.button
-                  to="/App"
-                  onClick={() => {
-                    props.setCustomerId(null);
-                  }}
-                >
-                  Log out
-                </s.button>{" "}
-                <s.header>{props.customerId}</s.header>
-              </div>
-            )}
-            <s.button to="/basket">
-              <RiShoppingCart2Line size={32} />
-            </s.button>
-          </s.menuContainer>
-        )}
-      </s.headerWrapper>
-      <s.underBanner/>
+            </s.menuContainer>
+          )}
+        </s.headerWrapper>
+        <s.underBanner />
       </div>
     );
   }
 
   return (
     <div>
-    <s.topBanner/>
-    <s.headerContainer>
-      <s.header>
-        THE H<s.logo>AR</s.logo>T.
-      </s.header>
-      
-      <s.buttonContainer>
-        <s.button to="/">Home</s.button>
-        {options.map((op) => (
-          <s.button key={op} to={`/${op}`}>
-            {op}
-          </s.button>
-        ))}
-        {props.customerId === null ? (
-          <s.button to="/login">Log in</s.button>
-        ) : (
-          <s.button
-            to="/"
-            onClick={() => {
-              props.setCustomerId(null);
-            }}
-          >
-            Log out
-          </s.button>
-        )}
-        {props.customerId === null ? (
-          <s.button to="/signup">Sign Up</s.button>
-        ) : (
-          <s.button to="/basket">
-            <RiShoppingCart2Line size={32} />
-          </s.button>
-        )}
-      </s.buttonContainer>
-    </s.headerContainer>
-    <s.underBanner/>
+      <s.topBanner />
+      <s.headerContainer>
+        <s.header>
+          THE H<s.logo>AR</s.logo>T.
+        </s.header>
+
+        <s.buttonContainer>
+          <s.button to="/">Home</s.button>
+          {options.map((op) => (
+            <s.button key={op} to={`/${op}`}>
+              {op}
+            </s.button>
+          ))}
+          {props.customerId === null ? (
+            <s.button to="/login">Log in</s.button>
+          ) : (
+            <s.button
+              to="/"
+              onClick={() => {
+                props.setCustomerId(null);
+              }}
+            >
+              Log out
+            </s.button>
+          )}
+          {props.customerId === null ? (
+            <s.button to="/signup">Sign Up</s.button>
+          ) : (
+            <s.button to="/basket">
+              <RiShoppingCart2Line size={32} />
+            </s.button>
+          )}
+        </s.buttonContainer>
+      </s.headerContainer>
+      <s.underBanner />
     </div>
   );
 };
